e5286d32797c100fc88245157b06ec0ee79b50a8
imron-123 <lapiimron@gmail.com>
2021-03-23 10:47:37 +0700

    Menambahkan usaha dan doa

--- test.py
+++ test.py
@@ -3,3 +3,4 @@ print("Semoga hari sabtu belajar git dan github saya suadh selesai")
 print("Ayo semnagat")
 
 print("Lagi Lagi jangan pustus asa")
+print("Takkan ada sia sia usaha kita jika di iringi doa dan usaha")
